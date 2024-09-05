f6e705d377ca06763b9fe8c5862616ec42316a3a
kazma <kazma233@outlook.com>
2024-09-05 08:05:14 +0800

    feat: update

--- docker-compose/service/jenkins/build_env.sh
+++ docker-compose/service/jenkins/build_env.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 apt-get update && \
 apt-get install -y build-essential wget && \
 rm -rf /var/lib/apt/lists/* && \
-wget -O tinygo.deb https://github.com/tinygo-org/tinygo/releases/download/v0.33.0/tinygo_0.33.0_amd64.deb && \
-dpkg -i tinygo.deb && \
-rm -f tinygo.deb
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && \
+source $HOME/.cargo/env && \
+cargo install wasm-pack
